81558764acb0f49be5d6f508a1a6e3f53f409340
livoras <livoras@163.com>
2015-11-15 15:48:33 +0800

    removing items

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+/**
+ * Diff two list in O(N).
+ */
+
 function diff(oldList, newList, key) {
-  if (!key) throw new Error("Must provide a way to get items' keys!")
+  if (!key) throw new Error("List's items must have keys!")
   var oldMap = makeMap(oldList, key)
   var newMap = makeMap(newList, key)
+
+  var inserts = []
+  var removes = []
+  var moves = []
+
+  var list = oldList.slice(0)
+  var i = 0;
+
+  // first pass remove all items are not in new map
+  while(i < list.length) {
+    var item = list[i]
+    var itemKey = getItemKey(item, key)
+    if (!newMap.hasOwnProperty(itemKey)) {
+      list.splice(i, 1)
+      removes.push({index: i})
+    } else {
+      i++;
+    }
+  }
+
 }
 
 /**
@@ -13,13 +37,17 @@ function makeMap(list, key) {
   var map = {}
   for(var i = 0, len = list.length; i < len; i++) {
     var item = list[i]
-    if (typeof key === "string") {
-      map[item[key]] = i
-    } else {
-      map[key(item)] = i
-    }
+    var itemKey = getItemKey(item, key);
+    map[itemKey] = i;
   }
   return map
 }
 
+function getItemKey(item, key) {
+  return typeof key === "string"
+    ? item[key]
+    : key(item)
+}
+
 exports.makeMap = makeMap
+exports.diff = diff
--- test/list-diff.spec.js
+++ test/list-diff.spec.js
@@ -28,4 +28,12 @@ describe("List diff", function() {
     })
   })
 
+  it("diff", function() {
+    diff.diff(
+      [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}], 
+      [{id: 2}, {id: 3}, {id: 1}], 
+      "id"
+    )
+  })
+
 })
